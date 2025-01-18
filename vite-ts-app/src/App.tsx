46c44840fcc87b5302929b59da7f5eb667c15b20
Hira Siddiqui <12besehsiddiqui@seecs.edu.pk>
2025-01-18 18:44:24 +0100

    Fix typo of clientId

--- vite-ts-app/src/App.tsx
+++ vite-ts-app/src/App.tsx
@@ -15,7 +15,7 @@ import {
 const App = () => {
 
     // options for the embedded profiles wallet
-    const options = { cliendId: '', theme: 'light' };
+    const options = { clientId: '', theme: 'light' };
     
     const abi = '[{"inputs":[],"name":"retrieve","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"num","type":"uint256"}],"name":"store","outputs":[],"stateMutability":"nonpayable","type":"function"}]';
     const rawTx = JSON.stringify({
